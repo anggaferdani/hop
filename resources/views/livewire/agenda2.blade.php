8fefeaadb1c2fe55e867be02808c53f2c2412338
Angga Wahyu Ferdani <eanggawahyu@gmail.com>
2023-07-30 19:24:16 +0700

    agendas

--- resources/views/livewire/agenda2.blade.php
+++ resources/views/livewire/agenda2.blade.php
@@ -18,7 +18,7 @@
     </form>
     <div class="row g-2 mb-4">
       @foreach($agendas as $agenda)
-        <?php $lokasi = $agenda->provinsi.", ".$agenda->kabupaten_kota.", ".$agenda->kecamatan ?>
+        <?php $lokasi = $agenda->Provinsi->nama_provinsi.", ".$agenda->Kabupaten->nama_kabupaten.", ".$agenda->Kecamatan->nama_kecamatan ?>
           <div class="col-md-4">
             @if($agenda->tiket == 'Berbayar')
             <div class="card" style="background-color: #EC5D71;">
@@ -29,7 +29,7 @@
                 </div>
                 @endforeach
                 <div class="col-8 col-md-8">
-                  <div class="card-body" style="height: 200px; display: flex; justify-content: space-between; flex-direction: column;">
+                  <div class="card-body" style="height: 220px; display: flex; justify-content: space-between; flex-direction: column;">
                     <div>
                       <div class="d-flex mb-2 justify-content-between">
                         <div class="col-md-10">
@@ -44,7 +44,7 @@
                           @endif
                         </div>
                       </div>
-                      <div class="small text-white lh-sm deskripsi2 mb-2" style="text-align: justify; word-break: break-all;">{!! $agenda->deskripsi !!}</div>
+                      <div class="small text-white lh-sm mb-2" style="text-align: justify; word-break: break-all;">{!! Str::limit($agenda->deskripsi, 50) !!}</div>
                     </div>
                     <div>
                       <div class="d-flex gap-1 mb-2">
@@ -52,7 +52,7 @@
                           <div class="tagging3 rounded-2 py-1 px-2">{{ Str::limit($type->type, 15) }}</div>
                         @endforeach
                       </div>
-                      <div class="small mb-0 text-white" style="font-size: 11px;">{{ $agenda->Provinsi->nama_provinsi }}, {{ $agenda->Kabupaten->nama_kabupaten }}, {{ $agenda->Kecamatan->nama_kecamatan }}</div>
+                      <div class="small mb-0 text-white" style="font-size: 11px;">{{ Str::limit($lokasi, 65) }}</div>
                       <div class="small mb-0 text-white">{{ \Carbon\Carbon::parse($agenda->tanggal_mulai)->format('d M Y') }} - {{ \Carbon\Carbon::parse($agenda->tanggal_berakhir)->format('d M Y') }}</div>
                       <a href="{{ route('agenda', Crypt::encrypt($agenda->id)) }}" class="stretched-link"></a>
                     </div>
@@ -70,7 +70,7 @@
                 </div>
                 @endforeach
                 <div class="col-8 col-md-8">
-                  <div class="card-body" style="height: 200px; display: flex; justify-content: space-between; flex-direction: column;">
+                  <div class="card-body" style="height: 220px; display: flex; justify-content: space-between; flex-direction: column;">
                     <div>
                       <div class="d-flex mb-2 justify-content-between">
                         <div class="col-md-10">
@@ -85,7 +85,7 @@
                           @endif
                         </div>
                       </div>
-                      <div class="small color lh-sm deskripsi2 mb-2" style="text-align: justify; word-break: break-all;">{!! $agenda->deskripsi !!}</div>
+                      <div class="small color lh-sm mb-2" style="text-align: justify; word-break: break-all;">{!! Str::limit($agenda->deskripsi, 50) !!}</div>
                     </div>
                     <div>
                       <div class="d-flex gap-1 mb-2">
@@ -93,7 +93,7 @@
                           <div class="tagging rounded-2 py-1 px-2">{{ Str::limit($type->type, 15) }}</div>
                         @endforeach
                       </div>
-                      <div class="small mb-0 color2" style="font-size: 11px;">{{ $agenda->Provinsi->nama_provinsi }}, {{ $agenda->Kabupaten->nama_kabupaten }}, {{ $agenda->Kecamatan->nama_kecamatan }}</div>
+                      <div class="small mb-0 color2" style="font-size: 11px;">{{ Str::limit($lokasi, 65) }}</div>
                       <div class="small mb-0 color2">{{ \Carbon\Carbon::parse($agenda->tanggal_mulai)->format('d M Y') }} - {{ \Carbon\Carbon::parse($agenda->tanggal_berakhir)->format('d M Y') }}</div>
                       <a href="{{ route('agenda', Crypt::encrypt($agenda->id)) }}" class="stretched-link"></a>
                     </div>
