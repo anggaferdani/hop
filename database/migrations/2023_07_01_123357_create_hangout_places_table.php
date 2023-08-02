ca01dedf83914f8e278240c1a00d2d92f824076d
Angga Wahyu Ferdani <eanggawahyu@gmail.com>
2023-08-03 00:44:25 +0700

    hangout places

--- database/migrations/2023_07_01_123357_create_hangout_places_table.php
+++ database/migrations/2023_07_01_123357_create_hangout_places_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->string('kabupaten_kota');
             $table->string('kecamatan');
             $table->string('logo')->nullable();
-            $table->enum('harga', ['< = Rp.50.000', 'Rp.50.000 - Rp.100.000', '> = Rp.100.000'])->nullable();
+            $table->enum('harga', ['< = Rp.500.000', 'Rp.500.000 - Rp.1.000.000', '> = Rp.1.000.000'])->nullable();
             $table->enum('status', ['Food And Beverage', 'Lodging', 'Public Area']);
             $table->enum('status_aktif', ['Aktif', 'Tidak Aktif'])->default('Aktif');
             $table->string('created_by')->nullable();
