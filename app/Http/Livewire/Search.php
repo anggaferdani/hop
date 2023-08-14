0d861b237fb2ca021ee10c06c54d3f27af70e039
Angga Ferdani <eanggawahyu@gmail.com>
2023-08-14 14:38:49 +0700

    searching

--- app/Http/Livewire/Search.php
+++ app/Http/Livewire/Search.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\ActivityManajemen;
 use App\Models\Agenda;
+use App\Models\HangoutPlace;
+use App\Models\Update;
 use Livewire\Component;
 use Illuminate\Support\Facades\Crypt;
 
@@ -54,7 +57,14 @@ class Search extends Component
 
     public function updatedQuery()
     {
-        $this->agendas = Agenda::where('judul', 'like', '%' . $this->query . '%')->orWhere('deskripsi', 'like', '%' . $this->query . '%')->get()->toArray();
+        $agenda2 = Agenda::where('judul', 'like', '%' . $this->query . '%')->orWhere('deskripsi', 'like', '%' . $this->query . '%')->where('status_aktif', 'Aktif')->get();
+        $update2 = Update::where('judul', 'like', '%' . $this->query . '%')->orWhere('deskripsi', 'like', '%' . $this->query . '%')->where('status_aktif', 'Aktif')->get();
+        $food_and_beverage2 = HangoutPlace::where('nama_tempat', 'like', '%' . $this->query . '%')->where('status', 'Food And Beverage')->where('status_aktif', 'Aktif')->get();
+        $lodging2 = HangoutPlace::where('nama_tempat', 'like', '%' . $this->query . '%')->where('status', 'Lodging')->where('status_aktif', 'Aktif')->get();
+        $public_area2 = HangoutPlace::where('nama_tempat', 'like', '%' . $this->query . '%')->where('status', 'Public Area')->where('status_aktif', 'Aktif')->get();
+        $activity_manajemen2 = ActivityManajemen::where('judul', 'like', '%' . $this->query . '%')->orWhere('deskripsi', 'like', '%' . $this->query . '%')->where('status_aktif', 'Aktif')->get();
+
+        $this->agendas = $agenda2->union($update2)->union($food_and_beverage2)->union($lodging2)->union($public_area2)->union($activity_manajemen2);
     }
 
     public function render()
